1615d73b760f21309410ce0de44c4e8ddddcb3da
mahdi-vajdi <mahdivajdii@gmail.com>
2023-06-17 11:33:29 +0330

    feat: Auth with local strategy -Created local strategy for passport and and AuthGuard for it -Added functions for creating, verifying and loggin users -Added passord hashing - Created "CurrentUser" decorator

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -5,7 +5,8 @@ import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -31,6 +32,6 @@ import Joi from 'joi';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
